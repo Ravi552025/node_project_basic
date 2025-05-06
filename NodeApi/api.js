bf70431c9a174e2e352d9eae349556a653a02533
Ravi Singh <ravis681299@gmail.com>
2025-05-07 01:10:19 +0530

    new change for npm basic

---
+++ NodeApi/api.js
@@ -0,0 +1,11 @@
+const api = (dbh) => {
+    try {
+        return dbh;
+    } catch (error) {
+        console.log("jknvfd",error);
+        
+    }
+   
+}
+const inputvalue = "someValue"
+console.log("vnf",api(inputvalue));
