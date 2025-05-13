1d55fe4ade06fee8ee0e36726b632c6a3a67fa3d
Ravi Singh <ravis681299@gmail.com>
2025-05-14 01:10:33 +0530

    new changes

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 15513
+
+app.get('/', (req, res) => {
+    res.send("calling get method in express!")
+})
+
+app.listen(port, () => {
+    console.log(`port is running on ${port}`);
+    
+})
