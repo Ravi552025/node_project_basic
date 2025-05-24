fd1089f5baab606a701280f5cadd3b3884679dc2
Ravi Singh <ravis681299@gmail.com>
2025-05-25 00:30:02 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+import express from "express"
+import userRoutes from "../../node_project_basic/routes/users"
+import bodyParser from 'body-parser'
 
-app.get("/", function (req, res) {
-    res.send("bvjhbffffff")
-});
-app.listen(port, function () {
-    console.log(`running on port ${port}`);
+const app = express();
+const port = 15513
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("calling get method in express!")
+})
+
+app.listen(port, () => {
+    console.log(`port is running on ${port}`);
     
 })
