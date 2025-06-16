5231acccfae8d0722a2bd560acc3176e0c770d4a
ravi singh <ravis681299@gmail.com>
2025-06-16 19:41:12 +0530

    new changes

--- src/function.js
+++ src/function.js
@@ -3,11 +3,11 @@ import { MongoClient } from "mongodb";
 
 // const uri = 'mongodb://ravis681299:96LAG5eCEWF6jcAB@cluster0-shard-00-00.nsfqg.mongodb.net:27017,cluster0-shard-00-01.nsfqg.mongodb.net:27017,cluster0-shard-00-02.nsfqg.mongodb.net:27017/uvotake?ssl=true&replicaSet=atlas-nsfqg-shard-0&authSource=admin';
   //  MONGO_URI=mongodb://<username>:<password>@cluster0-shard-00-00.nsfqg.mongodb.net:27017,cluster0-shard-00-01.nsfqg.mongodb.net:27017,cluster0-shard-00-02.nsfqg.mongodb.net:27017/uvotake?ssl=true&replicaSet=atlas-nsfqg-shard-0&authSource=admin
-const uri = "mongodb+srv://ravis681299:96LAG5eCEWF6jcAB@cluster0.9bxb3v5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://ravis681299:96LAG5eCEWF6jcAB@cluster0.9bxb3v5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-console.log("bcjdf", uri);
+// console.log("bcjdf", uri);
 
-export const client = new MongoClient(uri)
+export const client = new MongoClient(process.env.DATABASE_URL)
 
 export async function connectToDB() {
   if (!client.topology || !client.topology.isConnected()) {
